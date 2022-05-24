49960779c0bba4acf9d96b004f5bb025e18b6406
Apple1501 <kunure15@gmail.com>
2022-05-24 22:10:43 +0300

    sql

--- tms/api.py
+++ tms/api.py
@@ -10,10 +10,14 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
 
     def list(self, request):
-        tasks = Task.objects.all()
+        user_id = self.request.query_params.get('id')
+        if user_id is not None:
+            tasks = Task.objects.filter(user=user_id)
+        if user_id is None:
+            tasks = Task.objects.all()
         tasks_serializer = TaskSerializer(tasks, many=True)
         return Response(tasks_serializer.data)
-
+    
     def retrieve(self, request, pk):
         task = Task.objects.get(id=pk)
         task_serializer = TaskSerializer(task)
@@ -23,9 +27,9 @@ class TaskViewSet(viewsets.ModelViewSet):
         title = request.data['title']
         description = request.data['description']
         is_active = request.data['is_active'] == 'true' if request.data['is_active'] else 'false'
-
-        Task.objects.create(title=title, description=description, is_active=is_active)
-        return Response(Task.objects.order_by('-pk')[0].pk)
+        user = request.data['user'] if request.data['user'] else 'null'
+        Task.objects.create(title=title, description=description, is_active=is_active,user=User.objects.get(id=user))
+        return Response("TaskSerializer.data")
 
     def delete(self, request, pk):
         task = self.get_object(pk)
@@ -57,7 +61,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
         User.objects.create(username=username, password=password,
                             fullname=fullname, group=group, role=role)
-        return Response(User.objects.order_by('-pk')[0])
+        return Response("User.objects.order_by('-pk')[0]")
 
     def delete(self, request, pk):
         user = self.get_object(pk)
--- tms/models.py
+++ tms/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-
-class Task(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    is_active = models.BooleanField()
-
-
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     fullname = models.CharField(max_length=50)
     group = models.CharField(max_length=50)
     role = models.CharField(max_length=50)
+
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    is_active = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
